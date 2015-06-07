5ace6974767ce961b7a73fb9df1e365c8cc2cc3e
Benoit Blanchon <github@benoitblanchon.fr>
2015-06-07 14:32:53 +0200

    Added support of Youtube videos

--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,38 @@
 chrome.runtime.sendMessage("show_page_action");
 
-function findVideos() {
+var youtubeRegex = /https:\/\/www\.youtube\.com\/(?:watch\?v=|embed\/)([a-zA-Z0-9]+)/;
+var youtubePluginUrl = "plugin://plugin.video.youtube/?action=play_video&videoid=$1";
+
+function findYoutubeVideosInLocation(result) {
+	var url1 = document.location.href;
+	var url2 = url1.replace(youtubeRegex, youtubePluginUrl)
+	if (url1 != url2) result.push(url2);
+}
+
+function findYoutubeVideosInIframes(result) {
+	var iframes = document.getElementsByTagName("iframe");
+	for(var i=0; i<iframes.length; i++) {
+		var url1 = iframes[i].src;
+		var url2 = url1.replace(youtubeRegex, youtubePluginUrl)
+		if (url1 != url2) result.push(url2);
+	}
+}
+
+
+function isBlobUrl(url) {
+	return url.indexOf("blob:") == 0;
+}
+
+function findHtml5Videos(result) {
 	var videos = document.getElementsByTagName("video");
-	var result = [];
 	for(var i=0; i<videos.length; i++) {
 		var video = videos[i];
-		if (video.src) result.push(video.src);
+		if (video.src && !isBlobUrl(video.src)) result.push(video.src);
 		var sources = video.getElementsByTagName("source");
 		for(var j=0; j<sources.length; j++) {
-			result.push(sources[j].src);
+			if (!isBlobUrl(sources[j].src)) {
+				result.push(sources[j].src);
+			}
 		}
 	}
 	return result;
@@ -16,6 +40,10 @@ function findVideos() {
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request == "get_videos") {
-		sendResponse(findVideos());
+		var result = [];
+		findHtml5Videos(result);
+		findYoutubeVideosInLocation(result);
+		findYoutubeVideosInIframes(result);
+		sendResponse(result);
 	}
 });
