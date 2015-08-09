a61990f7e5c580a08faa6b54f107cf245cc38294
Benoit Blanchon <github@benoitblanchon.fr>
2015-08-09 16:35:53 +0200

    Hide badge instead of writing '0'

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request.action == "set_videos") {
-		chrome.browserAction.setBadgeText({
-			"text": request.videos.length.toString(),
-			"tabId": sender.tab.id
-		});		
-		chrome.browserAction.setBadgeBackgroundColor({"color":"#31afe1"});
-	}
-});
+  if (request.action == "set_videos") {
+    var badge =
+        request.videos.length > 0 ? request.videos.length.toString() : '';
+    chrome.browserAction.setBadgeText({"text": badge, "tabId": sender.tab.id});
 
+    chrome.browserAction.setBadgeBackgroundColor({"color": "#31afe1"});
+  }
+});
--- popup.js
+++ popup.js
@@ -1,73 +1,65 @@
 var kodi;
 var elements;
 var storageArea = chrome.storage.local;
-storageArea.get(["url","username","password"], function(items) {
-	kodi = new Kodi(items.url, items.username, items.password);
+storageArea.get(["url", "username", "password"], function(items) {
+  kodi = new Kodi(items.url, items.username, items.password);
 });
 
 function onPlaySucceed() {
-	elements.statusContent.innerHTML = "<img src='img/spinner64.gif'>";
-	window.setTimeout(function() {
-			elements.statusOverlay.classList.add("hidden");
-		}, 5000);
-}
-
-function onPlaySucceed() {
-	elements.status.className = "success";
-	window.setTimeout(function() {
-			elements.status.className = "";
-			window.close();
-		}, 500);
+  elements.status.className = "success";
+  window.setTimeout(function() {
+    elements.status.className = "";
+    window.close();
+  }, 500);
 }
 
 function onPlayFailed() {
-	elements.status.className = "failure";
-	window.setTimeout(function() {
-			elements.status.className = "";
-		}, 2000);
+  elements.status.className = "failure";
+  window.setTimeout(function() { elements.status.className = ""; }, 2000);
 }
 
 function play(url) {
-	elements.status.className = "working";
-	kodi.play(url, function(response) {
-		if (response == "OK") onPlaySucceed();
-		else onPlayFailed();
-	});
+  elements.status.className = "working";
+  kodi.play(url, function(response) {
+    if (response == "OK")
+      onPlaySucceed();
+    else
+      onPlayFailed();
+  });
 }
 
 function onVideoLinkClick(event) {
-	play(event.target.href);
+  play(event.target.href);
 }
 
 function onPlayUrlClick() {
-	play(elements.url.value);
+  play(elements.url.value);
 }
 
 function onVideoListReceived(videos) {
-	var list = document.getElementById("videos");
+  var list = document.getElementById("videos");
 
-	for(var i=0; i<videos.length; i++) {
-		var item = document.createElement("li");
-		item.className = videos[i].type;
-		var anchor = document.createElement("a");
-		anchor.href = videos[i].url;
-		anchor.innerText = videos[i].title;
-		anchor.addEventListener("click", onVideoLinkClick);	
-		item.appendChild(anchor)
-		list.appendChild(item);
-	}
+  for (var i = 0; i < videos.length; i++) {
+    var item = document.createElement("li");
+    item.className = videos[i].type;
+    var anchor = document.createElement("a");
+    anchor.href = videos[i].url;
+    anchor.innerText = videos[i].title;
+    anchor.addEventListener("click", onVideoLinkClick);
+    item.appendChild(anchor) list.appendChild(item);
+  }
 }
 
 function onLoad() {
-	elements = {
-		play: document.getElementById("play"),
-		status: document.getElementById("status"),
-		url: document.getElementById("url")
-	};
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id,"get_videos", onVideoListReceived);
-    });
-    elements.play.addEventListener("click", onPlayUrlClick);
+  elements = {
+    play: document.getElementById("play"),
+    status: document.getElementById("status"),
+    url: document.getElementById("url")
+  };
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, "get_videos", onVideoListReceived);
+  });
+  elements.play.addEventListener("click", onPlayUrlClick);
 }
 
 window.addEventListener('DOMContentLoaded', onLoad);
